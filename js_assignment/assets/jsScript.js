3e67af03f1c051613f9f667be8f2d3b84cf3842f
dmookken-QB <donatto@qburst.com>
2020-10-29 01:09:22 +0530

    Initial commit. Implemented calculator.

---
+++ js_assignment/assets/jsScript.js
@@ -0,0 +1,47 @@
+var displayInputString = ''
+var processInputString = '';
+var answer = 0;
+var previousOperator = '+';
+var presentNumber = 0;
+var presentDigitNumber = 0;
+var presetDecimalNumber = 0;
+var presentOperator = '+';
+var previousAnswer = 0.0
+var evaluateExpression = '';
+
+
+function buttonPressed(buttonKey) {
+    if(buttonKey == 'c') {
+        console.log('Screen cleared.');
+        document.getElementById("answer").innerHTML = '0';
+        document.getElementById("input").innerHTML = '';
+        answer = 0;
+        previousOperator = '+';
+        presentNumber = 0;
+        previousAnswer = 0.0
+        return;
+    }
+
+    processInputString = processInputString.concat(buttonKey);
+
+    if(buttonKey == '/' || buttonKey == '*' || buttonKey == '-' || buttonKey == '+') {
+        previousOperator = buttonKey;
+        presentNumber = 0;
+        $("#input").append(` <span>${buttonKey}</span> `);
+        return;
+    }
+    else {
+        buttonParseKey = buttonKey;
+        if(buttonKey === '.')
+            buttonParseKey = '0.0'
+
+        presentNumber = presentNumber * 10 + parseInt(buttonParseKey)
+        evaluateExpression = `${previousAnswer}${previousOperator}${presentNumber}`
+        console.log(evaluateExpression)
+        previousAnswer = eval(evaluateExpression)
+
+        // console.log(processInputString, " = ", previousAnswer);
+        document.getElementById("answer").innerHTML = previousAnswer;
+        $("#input").append(`${buttonKey}`);
+    }
+}
